80a1b1955fca3d5fe704999494e239c1a1f151d0
myfreeer <myfreeer@users.noreply.github.com>
2017-03-09 10:18:32 +0800

    clean code

--- enableallExtensions.cmd
+++ enableallExtensions.cmd
@@ -18,9 +18,9 @@ title Add All Existing Chrome Extensions To ExtensionInstallWhitelist
 if exist rule.txt del rule.txt
 set /a n=0
 call :addbase
-if exist "User Data" FOR /F "usebackq" %i IN (`dir /AD /B "User Data"`) DO @if exist "User Data\%~i\Extensions" FOR /F "usebackq" %j IN (`dir /AD /B "User Data\%~i\Extensions"`) do call :addrule %%i
-if not ["%CD%"] == ["%LOCALAPPDATA%\Google\Chrome"] if exist "%LOCALAPPDATA%\Google\Chrome\User Data" FOR /F "usebackq" %i IN (`dir /AD /B "%LOCALAPPDATA%\Google\Chrome\User Data"`) DO @if exist "%LOCALAPPDATA%\Google\Chrome\User Data\%~i\Extensions" FOR /F "usebackq" %j IN (`dir /AD /B "%LOCALAPPDATA%\Google\Chrome\User Data\%~i\Extensions"`) do call :addrule %%i
-if not defined LOCALAPPDATA if not ["%CD%"] == ["%USERPROFILE%\Local Settings\Application Data\Google\Chrome"] if exist "%USERPROFILE%\Local Settings\Application Data\Google\Chrome\User Data" FOR /F "usebackq" %i IN (`dir /AD /B "%USERPROFILE%\Local Settings\Application Data\Google\Chrome\User Data"`) DO @if exist "%USERPROFILE%\Local Settings\Application Data\Google\Chrome\User Data\%~i\Extensions" FOR /F "usebackq" %j IN (`dir /AD /B "%USERPROFILE%\Local Settings\Application Data\Google\Chrome\User Data\%~i\Extensions"`) do call :addrule %%i
+if exist "User Data" call :adduserdata "User Data"
+if not ["%CD%"] == ["%LOCALAPPDATA%\Google\Chrome"] if exist "%LOCALAPPDATA%\Google\Chrome\User Data" call :adduserdata "%LOCALAPPDATA%\Google\Chrome\User Data"
+if not defined LOCALAPPDATA if not ["%CD%"] == ["%USERPROFILE%\Local Settings\Application Data\Google\Chrome"] if exist "%USERPROFILE%\Local Settings\Application Data\Google\Chrome\User Data" call :adduserdata "%USERPROFILE%\Local Settings\Application Data\Google\Chrome\User Data"
 if exist rule.txt LGPO.exe /t rule.txt
 ::if exist rule.txt move rule.txt nul
 if exist rule.txt del /f /q rule.txt
@@ -43,3 +43,9 @@ echo Software\Policies\Google\Chrome\ExtensionInstallWhitelist>>rule.txt
 echo ^%n%>>rule.txt
 echo SZ:%1>>rule.txt
 exit /B
+
+:adduserdata
+if [%1]==[] exit /B -1
+if nor exist "%~1" exit /B -1
+FOR /F "usebackq" %%i IN (`dir /AD /B "%~1"`) DO @if exist "%~1\%%~i\Extensions" FOR /F "usebackq" %j IN (`dir /AD /B "%~1\%%~i\Extensions"`) do call :addrule %%i
+exit /B %ERRORLEVEL%
