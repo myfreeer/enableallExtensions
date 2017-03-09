e9e319afe94a91f92de6860eda99101dd9fe0aad
myfreeer <myfreeer@users.noreply.github.com>
2017-03-09 10:11:22 +0800

    support chrome's default user data folder
    automatically search for extensions in chrome's default user data folder

--- enableallExtensions.cmd
+++ enableallExtensions.cmd
@@ -18,14 +18,14 @@ title Add All Existing Chrome Extensions To ExtensionInstallWhitelist
 if exist rule.txt del rule.txt
 set /a n=0
 call :addbase
-FOR /F "usebackq" %i IN (`dir /AD /B "User Data"`) DO @if exist "User Data\%~i\Extensions" FOR /F "usebackq" %j IN (`dir /AD /B "User Data\%~i\Extensions"`) do call :addrule %%i
+if exist "User Data" FOR /F "usebackq" %i IN (`dir /AD /B "User Data"`) DO @if exist "User Data\%~i\Extensions" FOR /F "usebackq" %j IN (`dir /AD /B "User Data\%~i\Extensions"`) do call :addrule %%i
+if not ["%CD%"] == ["%LOCALAPPDATA%\Google\Chrome"] if exist "%LOCALAPPDATA%\Google\Chrome\User Data" FOR /F "usebackq" %i IN (`dir /AD /B "%LOCALAPPDATA%\Google\Chrome\User Data"`) DO @if exist "%LOCALAPPDATA%\Google\Chrome\User Data\%~i\Extensions" FOR /F "usebackq" %j IN (`dir /AD /B "%LOCALAPPDATA%\Google\Chrome\User Data\%~i\Extensions"`) do call :addrule %%i
+if not defined LOCALAPPDATA if not ["%CD%"] == ["%USERPROFILE%\Local Settings\Application Data\Google\Chrome"] if exist "%USERPROFILE%\Local Settings\Application Data\Google\Chrome\User Data" FOR /F "usebackq" %i IN (`dir /AD /B "%USERPROFILE%\Local Settings\Application Data\Google\Chrome\User Data"`) DO @if exist "%USERPROFILE%\Local Settings\Application Data\Google\Chrome\User Data\%~i\Extensions" FOR /F "usebackq" %j IN (`dir /AD /B "%USERPROFILE%\Local Settings\Application Data\Google\Chrome\User Data\%~i\Extensions"`) do call :addrule %%i
 if exist rule.txt LGPO.exe /t rule.txt
 ::if exist rule.txt move rule.txt nul
 if exist rule.txt del /f /q rule.txt
-pause
+timeout /t 60||pause
 exit /B 0
-exit /B 1
-exit /B -1
 
 :addbase
 echo Computer>rule.txt
